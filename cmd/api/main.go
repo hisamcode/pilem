8536f2bfb8eb0d18ffc8cabdd479a3f90026e1a5
hisamcode <hisamcode@gmail.com>
2024-08-17 19:00:45 +0700

    chore: use openDB at root main func rather than use in internal package reuse instancedb, and benefit is can use stub sqlmock.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,54 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
+	"os"
 	"pilem/internal/server"
+
+	"github.com/joho/godotenv"
 )
 
+type config struct {
+	db struct {
+		dsn string
+	}
+}
+
 func main() {
+	var cfg config
 
-	server := server.NewServer()
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 
-	err := server.ListenAndServe()
+	// database config
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_DSN"), "PostgreSQL DSN")
+
+	flag.Parse()
+
+	// OpenDB
+	db, err := openDB(cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	server := server.NewServer(db)
+
+	err = server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("pgx", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
