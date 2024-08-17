c07cd22f8b248f4fe6503b78827d7f125b5873a4
hisamcode <hisamcode@gmail.com>
2024-08-17 19:15:00 +0700

    feat: Create CreateMovieHandler
    chore: commented code healthHandler

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"pilem/helper"
+	"pilem/internal/data"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
@@ -29,11 +31,45 @@ func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
-	jsonResp, err := json.Marshal(s.db.Health())
+	// jsonResp, err := json.Marshal(s.db.Health())
 
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	// if err != nil {
+	// 	log.Fatalf("error handling JSON marshal. Err: %v", err)
+	// }
+
+	// _, _ = w.Write(jsonResp)
+}
+
+func (s *Server) CreateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
-	_, _ = w.Write(jsonResp)
+	err := helper.ReadJSON(w, r, &input)
+	if err != nil {
+		helper.BadRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	err = s.db.Movies.Insert(movie)
+	if err != nil {
+		helper.ServerErrorResponse(w, r, err)
+		return
+	}
+
+	err = helper.WriteJSON(w, http.StatusCreated, helper.Envelope{"movie": movie}, nil)
+	if err != nil {
+		helper.ServerErrorResponse(w, r, err)
+	}
+
 }
