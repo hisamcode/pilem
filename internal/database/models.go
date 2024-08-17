dba1d5754ae0132bfb0dceb84b410871accbaa2f
hisamcode <hisamcode@gmail.com>
2024-08-17 19:11:52 +0700

    feat: centerelize models

---
+++ internal/database/models.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"database/sql"
+)
+
+type Models struct {
+	Movies MovieModel
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+	}
+}
