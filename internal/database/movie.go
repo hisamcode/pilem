fdc5aeda0e76382e8db6e70590591306ae1b8246
hisamcode <hisamcode@gmail.com>
2024-08-20 12:06:06 +0700

    feat: adding Delete movie function

--- internal/database/movie.go
+++ internal/database/movie.go
@@ -66,3 +66,33 @@ func (m MovieModel) Get(id int64) (*data.Movie, error) {
 
 	return &movie, nil
 }
+
+func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+	DELETE FROM movies
+	WHERE id = $1
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	result, err := m.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
+	return nil
+}
